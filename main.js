5c6704c8d9e85fc5e9fd687750ca85fb55532c07
Wonyoung Choi <wy80.choi@samsung.com>
2018-01-26 14:29:53 +0900

    Dectect base ref branch

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-var log = require('./log')
-var express = require('express')
-var request = require('request')
-var webhook = require('express-github-webhook')
-var parser = require('body-parser')
+var log = require('./log');
+var express = require('express');
+var request = require('request');
+var webhook = require('express-github-webhook');
+var parser = require('body-parser');
 
-var accessToken = process.env.GITHUB_TOKEN
+var accessToken = process.env.GITHUB_TOKEN;
 var webhookHandler = webhook({
   path: '/',
   secret: process.env.GITHUB_SECRET
-})
+});
 
-var app = express()
-app.set('port', process.env.PORT || 5555)
-app.use(parser.json())
-app.use(webhookHandler)
+var app = express();
+app.set('port', process.env.PORT || 5555);
+app.use(parser.json());
+app.use(webhookHandler);
 
 
 webhookHandler.on('pull_request', function(repo, data) {
-  log.info('repo=' + repo);
-})
+  if (repo !== 'TizenFX' || data.action !== 'opened') return;
+
+  var labels = [];
+
+  // Label for API Version
+  if (data.pull_request.base.ref === 'master') {
+    labels.push('API5');
+  } else if (data.pull_request.base.ref === 'API4') {
+    labels.push('API4');
+  }
+
+  log.info('adding lables ' + JSON.stringify(lables) + ' to ' + data.pull_request.number);
+
+});
 
 
 app.listen(app.get('port'), function() {
   log.info('tizenfx-ghtools is listening on port ' + app.get('port'))
-})
+});
+
